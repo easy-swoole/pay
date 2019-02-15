db9feb23b922dd90d443507ab654a93c95964fd0
yf <291323003@qq.com>
2019-02-15 22:16:55 +0800

    up

--- src/AliPay/Config.php
+++ src/AliPay/Config.php
@@ -13,5 +13,106 @@ use EasySwoole\Spl\SplBean;
 
 class Config extends SplBean
 {
+    protected $appId;
+    protected $notifyUrl;
+    protected $returnUrl;
+    protected $publicKey;
+    protected $privateKey;
+    protected $gateWay = GateWay::SANDBOX;
 
+    /**
+     * @return mixed
+     */
+    public function getAppId()
+    {
+        return $this->appId;
+    }
+
+    /**
+     * @param mixed $appId
+     */
+    public function setAppId($appId): void
+    {
+        $this->appId = $appId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNotifyUrl()
+    {
+        return $this->notifyUrl;
+    }
+
+    /**
+     * @param mixed $notifyUrl
+     */
+    public function setNotifyUrl($notifyUrl): void
+    {
+        $this->notifyUrl = $notifyUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReturnUrl()
+    {
+        return $this->returnUrl;
+    }
+
+    /**
+     * @param mixed $returnUrl
+     */
+    public function setReturnUrl($returnUrl): void
+    {
+        $this->returnUrl = $returnUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPublicKey()
+    {
+        return $this->publicKey;
+    }
+
+    /**
+     * @param mixed $publicKey
+     */
+    public function setPublicKey($publicKey): void
+    {
+        $this->publicKey = $publicKey;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrivateKey()
+    {
+        return $this->privateKey;
+    }
+
+    /**
+     * @param mixed $privateKey
+     */
+    public function setPrivateKey($privateKey): void
+    {
+        $this->privateKey = $privateKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGateWay(): string
+    {
+        return $this->gateWay;
+    }
+
+    /**
+     * @param string $gateWay
+     */
+    public function setGateWay(string $gateWay): void
+    {
+        $this->gateWay = $gateWay;
+    }
 }
