b664310dcd08b239ff26e12a9aeb7f34ef0a1289
yf <291323003@qq.com>
2019-02-15 12:02:12 +0800

    up

---
+++ src/Utility/NewWork.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-02-15
+ * Time: 12:00
+ */
+
+namespace EasySwoole\Pay\Utility;
+
+
+use EasySwoole\HttpClient\HttpClient;
+
+class NewWork
+{
+    public static $TIMEOUT = 15;
+
+    public static function get($url)
+    {
+        $client = new HttpClient();
+    }
+
+    public static function post()
+    {
+
+    }
+
+    public static function postJson()
+    {
+
+    }
+
+    public static function postXML()
+    {
+
+    }
+}
