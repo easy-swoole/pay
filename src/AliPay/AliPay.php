91601b882ec38a558f67e0ac4582a7516d818fbf
Tioncico <31308307+tioncico@users.noreply.github.com>
2019-05-07 10:47:25 +0800

    preQuest方法改为公共调用

--- src/AliPay/AliPay.php
+++ src/AliPay/AliPay.php
@@ -350,7 +350,7 @@ class AliPay
 	 * @throws \EasySwoole\Pay\Exceptions\GatewayException
 	 * @throws \EasySwoole\Pay\Exceptions\InvalidSignException
 	 */
-	private function preQuest( array $data ) : SplArray
+	public function preQuest( array $data ) : SplArray
 	{
 		$response = NewWork::post( $this->config->getGateWay(), $data );
 		$result = json_decode( mb_convert_encoding( $response->getBody(), 'utf-8', 'gb2312' ), true );
