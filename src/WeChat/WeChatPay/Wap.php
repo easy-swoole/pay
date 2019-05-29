cb9bb12c23e115dcb90379a1c6083de86614ddb7
evalor <mipone@foxmail.com>
2019-05-29 16:35:45 +0800

    仅当没有指定NotifyUrl时，使用全局设置

--- src/WeChat/WeChatPay/Wap.php
+++ src/WeChat/WeChatPay/Wap.php
@@ -8,23 +8,31 @@
 
 namespace EasySwoole\Pay\WeChat\WeChatPay;
 
+use EasySwoole\Pay\Exceptions\GatewayException;
+use EasySwoole\Pay\Exceptions\InvalidArgumentException;
+use EasySwoole\Pay\Exceptions\InvalidSignException;
 use EasySwoole\Pay\WeChat\RequestBean\Base;
+use EasySwoole\Pay\WeChat\RequestBean\PayBase;
 use EasySwoole\Pay\WeChat\ResponseBean\Wap as WapResponse;
 use EasySwoole\Pay\WeChat\Utility;
 
 class Wap extends AbstractPayBase
 {
     /**
-     * H5支付
-     * @param Base $bean
+     * 发起一次支付
+     * @param PayBase $bean
      * @return WapResponse
-     * @throws \EasySwoole\Pay\Exceptions\GatewayException
-     * @throws \EasySwoole\Pay\Exceptions\InvalidArgumentException
-     * @throws \EasySwoole\Pay\Exceptions\InvalidSignException
+     * @throws GatewayException
+     * @throws InvalidArgumentException
+     * @throws InvalidSignException
      */
     public function pay(Base $bean): WapResponse
     {
-        $bean->setNotifyUrl($this->config->getNotifyUrl());
+        // 如果没有定义回调 使用全局回调
+        if (empty($bean->getNotifyUrl())) {
+            $bean->setNotifyUrl($this->config->getNotifyUrl());
+        }
+
         $utility = new Utility($this->config);
         $result = $utility->requestApi($this->requestPath(), $bean);
         return new WapResponse((array)$result);
