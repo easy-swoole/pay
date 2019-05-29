b1893380560df0ba80fd3775e11bbe039a3eeddf
韩文博 <9476400@qq.com>
2019-05-29 14:13:30 +0800

    给app支付增加 attach    spbill_create_ip   openid 参数

--- src/WeChat/RequestBean/App.php
+++ src/WeChat/RequestBean/App.php
@@ -26,6 +26,21 @@ class App extends Base
 	 */
 	protected $body;
 
+	/**
+	 * @var string
+	 */
+	protected $attach;
+
+	/**
+	 * @var string 终端IP
+	 */
+	protected $spbill_create_ip;
+
+	/**
+	 * @var string
+	 */
+	protected $openid;
+
 
 	/**
 	 * @return string
@@ -73,5 +88,53 @@ class App extends Base
 	public function setBody( string $body ) : void
 	{
 		$this->body = $body;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAttach() : string
+	{
+		return $this->attach;
+	}
+
+	/**
+	 * @param string $attach
+	 */
+	public function setAttach( string $attach ) : void
+	{
+		$this->attach = $attach;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSpbillCreateIp() : string
+	{
+		return $this->spbill_create_ip;
+	}
+
+	/**
+	 * @param string $spbill_create_ip
+	 */
+	public function setSpbillCreateIp( string $spbill_create_ip ) : void
+	{
+		$this->spbill_create_ip = $spbill_create_ip;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getOpenid() : string
+	{
+		return $this->openid;
+	}
+
+	/**
+	 * @param string $openid
+	 */
+	public function setOpenid( string $openid ) : void
+	{
+		$this->openid = $openid;
 	}
 }
