4733e7b2b13a64b0d68c064b25f6e9453bc9391a
韩文博 <9476400@qq.com>
2019-05-29 16:53:42 +0800

    no message

--- src/WeChat/WeChatPay/App.php
+++ src/WeChat/WeChatPay/App.php
@@ -21,24 +21,21 @@ class App extends AbstractPayBase
 		return '/pay/unifiedorder';
 	}
 
-	public function pay( Base $bean )
+
+	public function pay(Base $bean)
 	{
-		/** @var \EasySwoole\Pay\WeChat\RequestBean\App $bean */
-		$utility = new Utility( $this->config );
-		// 如果没有定义回调 使用全局回调
-		if( empty( $bean->getNotifyUrl() ) ){
-			$bean->setNotifyUrl( $this->config->getNotifyUrl() );
-		}
-		$result       = $utility->requestApi( $this->requestPath(), $bean );
-		$data         = [
-			'appid'     => $result['appid'],
+		$bean->setNotifyUrl($this->config->getNotifyUrl());
+		$utility = new Utility($this->config);
+		$result = $utility->requestApi($this->requestPath(), $bean);
+		$data = [
+			'appid' => $result['appid'],
 			'partnerid' => $result['mch_id'],
-			'prepayid'  => $result['prepay_id'],
-			'noncestr'  => Random::character( 32 ),
-			'timestamp' => strval( time() ),
-			'package'   => 'Sign=WXPay',
+			'prepayid' => $result['prepay_id'],
+			'noncestr' => Random::character(32),
+			'timestamp' => strval(time()),
+			'package'=>'Sign=WXPay'
 		];
-		$data['sign'] = $utility->generateSign( $data );
-		return new AppResponse( $data );
+		$data['sign'] = $utility->generateSign($data);
+		return new AppResponse($data);
 	}
 }
