56977dffaf1704c141b6e7a3f0a182b4ce0bfd53
韩文博 <9476400@qq.com>
2019-06-09 00:26:18 +0800

    app支付增加$passback_params参数

--- src/AliPay/RequestBean/App.php
+++ src/AliPay/RequestBean/App.php
@@ -13,4 +13,26 @@ class App extends Base
 {
 	protected $product_code = 'QUICK_MSECURITY_PAY';
 	protected $method = 'alipay.trade.app.pay';
+	/**
+	 * @var string 支付宝的开发文档中规定passback_params的值需要经过Url编码后传递
+	 */
+	protected $passback_params;
+
+	/**
+	 * @return string
+	 */
+	public function getPassbackParams() : string
+	{
+		return $this->passback_params;
+	}
+
+	/**
+	 * @param string $passback_params
+	 */
+	public function setPassbackParams( string $passback_params ) : void
+	{
+		$this->passback_params = $passback_params;
+	}
+
+
 }
