cde9193d3404e7541891dc9a4fe2c71f5d3a6fac
韩文博 <9476400@qq.com>
2019-06-12 17:21:23 +0800

    no message

--- src/AliPay/RequestBean/Transfer.php
+++ src/AliPay/RequestBean/Transfer.php
@@ -11,7 +11,6 @@ namespace EasySwoole\Pay\AliPay\RequestBean;
 
 class Transfer extends Base
 {
-	protected $product_code;
 	protected $method = 'alipay.fund.trans.toaccount.transfer';
 	/**
 	 * @var string
