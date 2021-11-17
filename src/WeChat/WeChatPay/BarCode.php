c8418d4c13288c1d029aa00cf579b8986d56f094
giantwu <wujikong@ispied.com>
2021-11-17 17:14:42 +0800

    fix:完善可能是支付成功的场景

--- src/WeChat/WeChatPay/BarCode.php
+++ src/WeChat/WeChatPay/BarCode.php
@@ -25,22 +25,28 @@ class BarCode extends AbstractPayBase
 
         $result       = $utility->request( $this->requestPath(), $bean );
         $result = is_array($result) ? $result : $utility->fromXML($result);
-        if ((isset($result['return_code']) && $result['return_code'] == 'SUCCESS') && isset($result['err_code']) && $result['result_code'] == 'FAIL') {
-            // 以下情况 可能已经支付成功了,所以需要返回给调用者
-            if (in_array($result['err_code'], ['BANKERROR', 'SYSTEMERROR', 'USERPAYING'])) {
-                // 这里为了兼容微信的一个BUG，用请求的sign_type代替响应值里的sign_type
-                if (!isset($result['sign_type']) && $bean->getSignType() != null) {
-                    $result['sign_type'] = $bean->getSignType();
-                }
-                if ($utility->generateSign($result) === $result['sign']) {
-                    $resultArray = new SplArray($result);
-                    return new MicroPayResponse($resultArray->getArrayCopy());
-                }
-                throw new InvalidSignException('sign is error');
-            }
-        }
-        if (!isset($result['return_code']) || $result['return_code'] != 'SUCCESS' || $result['result_code'] != 'SUCCESS') {
+
+        //return_code:通信结果，所以必须是success，否则就是失败的。
+        if(!isset($result['return_code'])||$result['return_code']!='SUCCESS'){
             throw new GatewayException('Get Wechat API Error:' . ($result['return_msg'] ?? $result['retmsg']) . ($result['err_code_des'] ?? ''),$result,$result['return_code']);
         }
+        //当result_code 为fail时  err_code  为：BANKERROR, SYSTEMERROR, USERPAYING ,可能也是成功的，所以需要返回给调用方
+        if($result['result_code']=='FAIL')
+        {
+            if (!in_array($result['err_code'], ['BANKERROR', 'SYSTEMERROR', 'USERPAYING'])) {
+                throw new GatewayException('Get Wechat API Error:' . ($result['return_msg'] ?? $result['retmsg']) . ($result['err_code_des'] ?? ''),$result,$result['return_code']);
+            }
+        }
+        //这里为了兼容微信的一个BUG，用请求的sign_type代替响应值里的sign_type
+        if (!isset($result['sign_type']) && $bean->getSignType() != null) {
+            $result['sign_type'] = $bean->getSignType();
+        }
+        if ($utility->generateSign($result) === $result['sign']) {
+            $resultArray = new SplArray($result);
+            return new MicroPayResponse($resultArray->getArrayCopy());
+        }
+        throw new InvalidSignException('sign is error');
+
+
     }
 }
