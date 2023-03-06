f8e1bba37aa3753e8c927ea5c5bd8b8bdef9d7fc
如果的如果 <admin@fosuss.com>
2023-03-06 14:13:17 +0800

    1

--- src/Config/WechatConfig.php
+++ src/Config/WechatConfig.php
@@ -8,6 +8,10 @@ class WechatConfig extends SplBean
 {
     protected $mch_private_key;
 
+    protected $mch_cert_serial_no;
+
+    protected $mch_id;
+
     /**
      * @return mixed
      */
@@ -22,6 +26,38 @@ class WechatConfig extends SplBean
     public function setMchPrivateKey($mch_private_key): void
     {
         $this->mch_private_key = $mch_private_key;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMchCertSerialNo()
+    {
+        return $this->mch_cert_serial_no;
+    }
+
+    /**
+     * @param mixed $mch_cert_serial_no
+     */
+    public function setMchCertSerialNo($mch_cert_serial_no): void
+    {
+        $this->mch_cert_serial_no = $mch_cert_serial_no;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMchId()
+    {
+        return $this->mch_id;
+    }
+
+    /**
+     * @param mixed $mch_id
+     */
+    public function setMchId($mch_id): void
+    {
+        $this->mch_id = $mch_id;
     }
 
     protected function initialize(): void
--- src/Wechat.php
+++ src/Wechat.php
@@ -2,6 +2,7 @@
 
 namespace EasySwoole\Pay;
 
+use EasySwoole\HttpClient\HttpClient;
 use EasySwoole\Pay\Config\WechatConfig;
 use EasySwoole\Utility\Random;
 
@@ -17,23 +18,62 @@ class Wechat
     function jsApi()
     {
         $path = "/v3/pay/transactions/jsapi";
-        $json = json_encode([]);
+        $json = json_encode([
+            'appid'=>'wx47e12b9ddcc41b44',
+            'description'=>"测试",
+            'out_trade_no'=>"NO".time(),
+            'mchid'=>"1526422621",
+            'notify_url'=>"https://www.baidu.com",
+            'amount'=>[
+                'total'=>1,
+                'currency'=>"CNY"
+            ],
+            "payer"=>[
+                'openid'=>"o3Mk26HWFAGp6zlUpeyqlLFtYgu4"
+            ]
+        ]);
         $this->postRequest($path,$json);
     }
 
-    protected function getReuest(string $url)
+    function query(string $out_trade_no)
     {
+        $path = "/v3/pay/transactions/id/{$out_trade_no}?mchid={$this->config->getMchId()}";
+        $this->getReuest($path);
 
+    }
+
+
+    protected function getReuest(string $path)
+    {
+        $token = $this->sign("GET",$path,"");
+        $url = "https://api.mch.weixin.qq.com{$path}";
+        $client = new HttpClient($url);
+        $client->setHeader("Authorization",$token,false);
+        $resp = $client->get();
+        var_dump($resp->getBody());
     }
 
     protected function postRequest(string $path,string $json)
     {
+        $token = $this->sign("POST",$path,$json);
+        $url = "https://api.mch.weixin.qq.com{$path}";
+        $client = new HttpClient($url);
+        $client->setHeader("Authorization",$token,false);
+        $resp = $client->postJson($json);
+        var_dump($resp->getBody());
+    }
+
+
+    private function sign(string $method,string $path,string $body)
+    {
         $time = time();
-        $nonce = strtoupper( Random::character(16));
-        $body = "{POST}\n{$path}\n{$time}\n{$nonce}\n{$json}\n";
+        $nonce = strtoupper( Random::character(32));
+        $body = "{$method}\n{$path}\n{$time}\n{$nonce}\n{$body}\n";
         openssl_sign($body, $raw_sign, $this->config->getMchPrivateKey(), 'sha256WithRSAEncryption');
         $sign = base64_encode($raw_sign);
-
+        $schema = 'WECHATPAY2-SHA256-RSA2048 ';
+        return $schema.sprintf('mchid="%s",nonce_str="%s",timestamp="%d",serial_no="%s",signature="%s"',
+                $this->config->getMchId(), $nonce, $time, $this->config->getMchCertSerialNo(), $sign);
     }
 
 }
