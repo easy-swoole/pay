6fa9254a2e922c65aff832df38a9edb8744032d8
如果的如果 <admin@fosuss.com>
2024-07-22 16:18:15 +0800

    spl 依赖2.x

--- src/Config/AlipayConfig.php
+++ src/Config/AlipayConfig.php
@@ -2,7 +2,20 @@
 
 namespace EasySwoole\Pay\Config;
 
-class AlipayConfig
-{
+use EasySwoole\Pay\AliPay\GateWay;
+use EasySwoole\Spl\SplBean;
 
+class AlipayConfig extends SplBean
+{
+    protected $appId;
+    protected $notifyUrl;
+    protected $returnUrl;
+    protected $publicKey;
+    protected $privateKey;
+    protected $gateWay = GateWay::SANDBOX;
+    protected $charset = "utf-8";
+    protected $format = "JSON";
+    protected $signType = "RSA2";
+    protected $apiVersion = "1.0";
+    protected $appAuthToken;
 }
