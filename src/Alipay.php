20a65f693c4ea408cb4a0ac04b0fd2c5a181adaf
如果的如果 <admin@fosuss.com>
2024-07-26 20:38:37 +0800

    spl 依赖2.x

--- src/Alipay.php
+++ src/Alipay.php
@@ -5,6 +5,7 @@ namespace EasySwoole\Pay;
 use EasySwoole\Pay\Beans\Alipay\Gateway;
 use EasySwoole\Pay\Config\AlipayConfig;
 use EasySwoole\Pay\Request\Alipay\PreQrCode;
+use EasySwoole\Spl\SplFileStream;
 
 class Alipay
 {
@@ -25,5 +26,62 @@ class Alipay
     function preQrCode(PreQrCode $request)
     {
 
+    }
+
+    protected function getCertSN($certPath){
+        $string = new SplFileStream($certPath);
+        $cert = $string->__toString();
+        $ssl = openssl_x509_parse($cert);
+        return md5($this->array2string(array_reverse($ssl['issuer'])) . $ssl['serialNumber']);
+    }
+
+    protected function getRootCertSN($certPath)
+    {
+        $string = new SplFileStream($certPath);
+        $cert = $string->__toString();
+        $array = explode("-----END CERTIFICATE-----", $cert);
+        $SN = null;
+        for ($i = 0; $i < count($array) - 1; $i++) {
+            $ssl[$i] = openssl_x509_parse($array[$i] . "-----END CERTIFICATE-----");
+            if (strpos($ssl[$i]['serialNumber'], '0x') === 0) {
+                $ssl[$i]['serialNumber'] = $this->hex2dec($ssl[$i]['serialNumberHex']);
+            }
+            if ($ssl[$i]['signatureTypeLN'] == "sha1WithRSAEncryption" || $ssl[$i]['signatureTypeLN'] == "sha256WithRSAEncryption") {
+                if ($SN == null) {
+                    $SN = md5($this->array2string(array_reverse($ssl[$i]['issuer'])) . $ssl[$i]['serialNumber']);
+                } else {
+
+                    $SN = $SN . "_" . md5($this->array2string(array_reverse($ssl[$i]['issuer'])) . $ssl[$i]['serialNumber']);
+                }
+            }
+        }
+        return $SN;
+    }
+
+
+    /**
+     * 0x转高精度数字
+     * @param $hex
+     * @return int|string
+     */
+    protected function hex2dec($hex)
+    {
+        $dec = 0;
+        $len = strlen($hex);
+        for ($i = 1; $i <= $len; $i++) {
+            $dec = bcadd($dec, bcmul(strval(hexdec($hex[$i - 1])), bcpow('16', strval($len - $i))));
+        }
+        return $dec;
+    }
+
+    protected function array2string($array)
+    {
+        $string = [];
+        if ($array && is_array($array)) {
+            foreach ($array as $key => $value) {
+                $string[] = $key . '=' . $value;
+            }
+        }
+        return implode(',', $string);
     }
 }
