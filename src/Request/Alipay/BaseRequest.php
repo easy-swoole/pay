6eecb8897fa3c38efa275532442c85ee6f0f50ed
如果的如果 <admin@fosuss.com>
2026-07-08 14:45:46 +0800

    fix

--- src/Request/Alipay/BaseRequest.php
+++ src/Request/Alipay/BaseRequest.php
@@ -32,4 +32,29 @@ class BaseRequest extends BaseBean
     public ?string $app_cert_sn;
 
     public ?string $alipay_root_cert_sn;
+
+
+    /*
+        balance  余额,
+        moneyFund	余额宝
+        coupon	红包
+        pcredit	花呗
+        pcreditpayInstallment	花呗分期
+        creditCard	信用卡
+        creditCardExpress	信用卡快捷
+        creditCardCartoon	信用卡卡通
+        credit_group	信用支付类型（包含信用卡卡通、信用卡快捷、花呗、花呗分期）
+        debitCardExpress	借记卡快捷
+        mcard	商户预存卡
+        pcard	个人预存卡
+        promotion	优惠（包含实时优惠+商户优惠）
+        voucher	营销券
+        point	积分
+        mdiscount	商户优惠
+        bankPay	网银
+        有多个渠道时用“,”分隔
+     */
+    public ?string $disable_pay_channels;
+
+    public ?string $enable_pay_channels;
 }
